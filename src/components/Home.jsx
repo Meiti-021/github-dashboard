5ce660ebeb1b7a275072828f3df3be4c43010048
Mahdi <meiti.edu@gmail.com>
2023-01-01 19:36:59 +0330

    update home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,13 +11,6 @@ const Home = () => {
         <img src={gitninja} alt="" className="home__foco-pic" />
       </figure>
       <div className="home__main">
-        <nav className="home__nav">
-          <ul className="home__nav-list">
-            <li className="home__nav-item">سرویس ها</li>
-            <li className="home__nav-item">درباره من</li>
-            <li className="home__nav-item">راهنمای سایت</li>
-          </ul>
-        </nav>
         <div className="home__hero">
           <div className="home__logo">
             <img src={octocat} alt="" className="home__logo-icon" />

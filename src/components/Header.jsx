1a5cb1ac552a459980854a37b5b62d730956c64d
Mahdi <meiti.edu@gmail.com>
2023-01-04 03:51:18 +0330

    update header section structure and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { FaArrowLeft, FaSearch } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="header">
-      <h1>i'm header</h1>
+      <div className="header__input-group">
+        <FaSearch className="header__icon" />
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="جستجوی دیگر کاربران گیتهاب برحسب نام کاربری"
+        />
+        <button className="header__search-btn">
+          <FaArrowLeft className="header__icon" />
+        </button>
+      </div>
     </div>
   );
 };

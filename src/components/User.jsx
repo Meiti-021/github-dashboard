7c7759111794beab1cdb8b81e7d32b835f81a689
Mahdi <meiti.edu@gmail.com>
2023-01-04 23:18:25 +0330

    add mock data file and followers mock array

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Account from "./Account";
 import Profile from "./Profile";
+import Followers from "./Followers";
 const User = () => {
   return (
     <div className="user">
       <Account />
       <div className="user__main">
         <Profile />
+        <Followers />
       </div>
     </div>
   );
--- src/contexts/Contexts.jsx
+++ src/contexts/Contexts.jsx
@@ -1,39 +1,5 @@
 import React, { useContext, useState } from "react";
-const mockdata = {
-  login: "Meiti-021",
-  id: 89641841,
-  node_id: "MDQ6VXNlcjg5NjQxODQx",
-  avatar_url: "https://avatars.githubusercontent.com/u/89641841?v=4",
-  gravatar_id: "",
-  url: "https://api.github.com/users/Meiti-021",
-  html_url: "https://github.com/Meiti-021",
-  followers_url: "https://api.github.com/users/Meiti-021/followers",
-  following_url:
-    "https://api.github.com/users/Meiti-021/following{/other_user}",
-  gists_url: "https://api.github.com/users/Meiti-021/gists{/gist_id}",
-  starred_url: "https://api.github.com/users/Meiti-021/starred{/owner}{/repo}",
-  subscriptions_url: "https://api.github.com/users/Meiti-021/subscriptions",
-  organizations_url: "https://api.github.com/users/Meiti-021/orgs",
-  repos_url: "https://api.github.com/users/Meiti-021/repos",
-  events_url: "https://api.github.com/users/Meiti-021/events{/privacy}",
-  received_events_url: "https://api.github.com/users/Meiti-021/received_events",
-  type: "User",
-  site_admin: false,
-  name: "Mahdi dehgani",
-  company: null,
-  blog: "",
-  location: null,
-  email: null,
-  hireable: true,
-  bio: "Front-End web developer",
-  twitter_username: null,
-  public_repos: 16,
-  public_gists: 0,
-  followers: 0,
-  following: 0,
-  created_at: "2021-08-27T11:44:17Z",
-  updated_at: "2023-01-02T12:30:13Z",
-};
+import { mockdata, followersMock } from "../database/mockData";
 
 const AppContext = React.createContext();
 
